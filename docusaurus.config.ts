d4274ee89e86df46a60e1611136de2a732e40ce5
Dokfritz <83727901+Dokfritz@users.noreply.github.com>
2025-12-09 07:02:28 +0100

    Update docusaurus.config.ts
    edited-swapped:
    production-url
    baseUrl
    organizationName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://philstollery.github.io',
+  url: 'https://dokfritz23.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/github-usaurus/',
+  baseUrl: '/fifty-hurts/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'philstollery', // Usually your GitHub org/user name.
-  projectName: 'github-usaurus', // Usually your repo name.
+  organizationName: 'Dokfritz23', // Usually your GitHub org/user name.
+  projectName: 'fifty-hurts', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
